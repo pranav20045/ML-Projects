acc2a0a224de0ae26732c24189008ad09aa2d702
pranav <badepranav2045@gmail.com>
2025-08-06 01:57:28 +0530

    eda and models training

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+from src.logger import loggings
  
 def error_message_details(error,error_details:sys):
     _,_,exc_tb=error_details.exc_info()
@@ -8,7 +9,7 @@ def error_message_details(error,error_details:sys):
 
 class CustomException(Exception):
     def __init__(self,error_message,error_details:sys):
-        super.__init__(error_message)
+        super().__init__(error_message)
         self.error_message=error_message_details(error_message,error_details=error_details)
 
     def __str__(self):
