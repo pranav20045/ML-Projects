20d11a707073d90c567d9e8d808ad273c92e95bf
pranav <badepranav2045@gmail.com>
2025-08-07 01:33:23 +0530

    Data Injestion

---
+++ src/components/data_injestion.py
@@ -0,0 +1,46 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+## this class will be used to take input from any source
+@dataclass
+class DataInjestionConfig:
+    train_data_path: str=os.path.join('artifacts','train.csv')
+    test_data_path: str=os.path.join('artifacts','test.csv')
+    raw_data_path: str=os.path.join("artifacts",'data.csv')
+
+class DataInjestion:
+    def __init__(self):
+        self.injestion_config=DataInjestionConfig()
+    
+    def initiate_data_injestion(self):
+        logging.info("Entered the data injestion mehtod or component")
+        # we can take input from any data source here 
+        try:
+            df=pd.read_csv('notebook\data\stud.csv')
+            logging.info('Read the dataset')
+
+            os.makedirs(os.path.dirname(self.injestion_config.train_data_path),exist_ok=True)
+            df.to_csv(self.injestion_config.raw_data_path,index=False,header=True)
+            logging.info("Train test split initialted")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            train_set.to_csv(self.injestion_config.train_data_path, index=False,header=True)
+            test_set.to_csv(self.injestion_config.test_data_path, index=False, header=True)
+
+            logging.info("Injestion of dtaa completed")
+
+            return (
+                self.injestion_config.train_data_path,
+                self.injestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+
+if __name__=="__main__":
+    obj=DataInjestion()
+    obj.initiate_data_injestion()
