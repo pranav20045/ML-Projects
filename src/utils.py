b4fc0178b4aab4c55518301b36c20fdbcf0d9bbd
pranav <badepranav2045@gmail.com>
2025-08-07 20:39:07 +0530

    hyper parameter tuning

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
@@ -17,7 +18,7 @@ def save_obj(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         '''X_train,y_train,X_test,y_test=train_test_split(
             X,y,test_size=0.2,random_state=42
@@ -27,8 +28,15 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
         report={}
         for i in range(len(list(models))):
             model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
+
+
+            #model.fit(X_train,y_train)
             y_train_pred=model.predict(X_train)
             y_test_pred=model.predict(X_test)
 
