69432b994ef59391cd02a56157bd1e72d27ccda2
pranav <badepranav2045@gmail.com>
2025-08-07 15:17:59 +0530

    data transformation and injestion done

--- src/components/data_injestion.py
+++ src/components/data_injestion.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
 
 ## this class will be used to take input from any source
 @dataclass
@@ -43,4 +44,7 @@ class DataInjestion:
 
 if __name__=="__main__":
     obj=DataInjestion()
-    obj.initiate_data_injestion()
+    train_data,test_data=obj.initiate_data_injestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
